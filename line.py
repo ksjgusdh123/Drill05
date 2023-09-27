eb1875215bb00650c79f414c7592fa408164301a
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-09-27 18:53:08 +0900

    9/27일 수업

--- line.py
+++ line.py
@@ -1,6 +1,6 @@
 import turtle
 import random
-
+import math
 
 def stop():
     turtle.bye()
@@ -51,14 +51,38 @@ def draw_point(p):
 
 
 def draw_line(p1, p2):
-    # fill here
+    draw_big_point(p1)
+    draw_big_point(p2)
+
+    x1, y1 = p1[0], p1[1]   # (-100, -100)
+    x2, y2 = p2[0], p2[1]
+
+    for i in range(0, 100 + 1, 5):
+        t = i / 100
+        x = (1 - t) * x1 + t * x2
+        y = (1 - t) * y1 + t * y2
+        draw_point((x,y))
+
+    draw_point(p2)
+
     pass
 
 
 prepare_turtle_canvas()
 
+points = [(random.randint(-300, 300), random.randint(-300, 300)) for i in range(10)]
+
+# points = [(- 300, 200), (400, 350), (300, -300), (-200, -200)]
+
+for p in points:
+    draw_big_point(p)
+
+for i in range(0, len(points) - 1):
+    draw_line(points[i], points[i + 1])
+draw_line(points[-1], points[0])
 
 # fill here
-
+# draw_line((-100, -100), (300, 150))
+# draw_line((-100, -100), (-100, 300))
 
 turtle.done()
