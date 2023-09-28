772121c4c4dec6db8c5b623c1a8e5ed0ee42fc46
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-09-28 20:24:55 +0900

    간단한 틀을 잡아 화면에 손가락 나오게 하였다

--- move_character_with_hand.py
+++ move_character_with_hand.py
@@ -1,13 +1,11 @@
-import random
-
 from pico2d import *
 
-TUK_WIDTH, TUK_HEIGHT = 800, 600
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-hand = load_image('hand_arrow.png')
+
 
 def handle_events():
     global running
@@ -16,32 +14,26 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
+
+
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-hand_point = [0, 0]
 frame = 0
-boy_run = False
-
+hide_cursor()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-
-    if boy_run:
-        pass
-    else:
-        hand_point = [random.randint(- 300, 300), random.randint(-300, 300)]
-        boy_run = True
-
-    hand.draw(hand_point[0], hand_point[1])
     update_canvas()
     frame = (frame + 1) % 8
-    delay(0.05)
+
     handle_events()
 
 close_canvas()
